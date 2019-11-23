15fd9023ff34827e2f34511b6c7792633f3fded0
Ubuntu <ubuntu@ip-172-31-83-228.ec2.internal>
2019-11-23 16:09:16 +0000

    add boggle solver part two

--- boggle.py
+++ boggle.py
@@ -53,6 +53,7 @@ def path_to_word(grid,path):
     """
     
     return ''.join(grid[p] for p in path)
+
     
 def search(grid,dictionary):
     """
@@ -60,11 +61,14 @@ def search(grid,dictionary):
     """
     neighbours = all_grid_neighbours(grid)
     paths=[]
+    full_words, stems = dictionary
     
     def do_search(path):
         word = path_to_word(grid,path)
-        if word in dictionary:
+        if word in full_words:
             paths.append(path)
+        if word not in stems:
+            return
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path+[next_pos])
@@ -80,9 +84,16 @@ def get_dictionary(dictionary_file):
     """
     Load dictionary
     """
+    full_words,stems=set(),set()
     
     with open(dictionary_file) as f:
-        return [w.strip().upper() for w in f]
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+            
+            for i in range(1,len(word)):
+                stems.add(word[:i])
+    return full_words, stems
         
 def display_words(words):
     for word in words:
@@ -94,7 +105,7 @@ def main():
     This is the function that will run the whole project
     """
     
-    grid = make_grid(2,2)
+    grid = make_grid(4,4)
     dictionary = get_dictionary('words.txt')
     words = search(grid,dictionary)
     # for word in words:
